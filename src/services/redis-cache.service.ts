88cafafa5ca57ad79e3096e8caf0e55ddb932534
Kaique Cavalcanti <kaique.cavalcanti@comerc.com.br>
2024-02-25 19:04:56 -0300

    redis cache service

---
+++ src/services/redis-cache.service.ts
@@ -0,0 +1,22 @@
+import { CacheServiceInterface } from '../interfaces';
+import { Redis } from 'ioredis';
+
+export class RedisCacheService implements CacheServiceInterface {
+  private readonly redisClient: Redis;
+
+  constructor(host: string, port: number, password?: string) {
+    this.redisClient = new Redis({
+      host: host,
+      port: port,
+      password: password,
+    });
+  }
+
+  async set(key: string, value: string): Promise<void> {
+    await this.redisClient.set(key, value);
+  }
+
+  async get(key: string): Promise<any | null> {
+    return this.redisClient.get(key);
+  }
+}
