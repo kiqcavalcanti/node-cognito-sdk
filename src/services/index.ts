e0a821def6f4c9705de500fb1c7e353076a47dfe
Kaique Cavalcanti <kaique.cavalcanti@comerc.com.br>
2024-02-25 19:07:47 -0300

    fix import

--- src/services/index.ts
+++ src/services/index.ts
@@ -3,3 +3,4 @@ export * from './cognito-user.service';
 export * from './token-validation.service';
 export * from './cognito-base.service';
 export * from './cognito-group.service';
+export * from './redis-cache.service';
